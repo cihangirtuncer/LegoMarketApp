e89d8be3aded04ed28eeda96b41c9fb1661eedcf
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-21 17:38:26 +0300

    push

--- lib/core/constant/column/payment_column.dart
+++ lib/core/constant/column/payment_column.dart
@@ -9,76 +9,67 @@ BuildPaymentColumn(String price, String name, String explanation) {
       title: Text("Payment"),
       backgroundColor: Colors.green.shade700,
     ),
-    body: Padding(
-      padding: const EdgeInsets.fromLTRB(8, 12, 8, 8),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12), color: Colors.white),
-              width: 120,
-              height: 80,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          name,
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          explanation,
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      price,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ],
-                ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12), color: Colors.white),
+            width: 120,
+            height: 80,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        name,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        explanation,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    price,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12), color: Colors.white),
-              child: ButtonPayment(),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12), color: Colors.white),
+            child: ButtonPayment(),
+          ),
+        ),
+        Align(
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Add to box',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(Colors.green.shade700),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  color: Colors.green.shade700),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.all(16.0),
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Add to box',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+        )
+      ],
     ),
   );
 }
