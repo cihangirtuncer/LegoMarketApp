550b731676edfda9e6ae04ab7a5749c5e66235a8
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-25 23:56:41 +0300

    push

--- lib/core/components/orders_page/build_orders.dart
+++ lib/core/components/orders_page/build_orders.dart
@@ -5,6 +5,9 @@ import 'package:lego_market_app/core/constant/card/orders_card.dart';
 BuildOrders(BuildContext context) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [BuildOrdersCard("Fetucini Alfredo")],
+    children: [
+      BuildOrdersCard("Fetucini Alfredo"),
+      BuildOrdersCard("chicken"),
+    ],
   );
 }
