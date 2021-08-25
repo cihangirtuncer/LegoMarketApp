f4acaba1d68ad0c22ff210611e8de736b72f05b2
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-25 15:54:54 +0300

    push

--- lib/core/components/orders_page/build_orders.dart
+++ lib/core/components/orders_page/build_orders.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
 BuildOrders(BuildContext context) {
-  return Center(child: Text("This is orders tab"));
+  return ListView(
+    children: [
+      Text("This is orders tab"),
+    ],
+  );
 }
