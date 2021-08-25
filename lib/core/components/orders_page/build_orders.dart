c2757c58415252b2d47b9b3603841b6466cfe11d
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-25 18:05:22 +0300

    push

--- lib/core/components/orders_page/build_orders.dart
+++ lib/core/components/orders_page/build_orders.dart
@@ -7,9 +7,18 @@ BuildOrders(BuildContext context) {
     children: [
       Card(
         child: ListTile(
-          title: Text("Fetucini Alfredo"),
-          subtitle: Text("chicken,parmesan,cream"),
-          leading: Text("30"),
+          title: Text(
+            "Fetucini Alfredo",
+            style: TextStyle(fontSize: 20),
+          ),
+          subtitle: Text(
+            "chicken,parmesan,cream",
+            style: TextStyle(fontSize: 18),
+          ),
+          trailing: Text(
+            "30",
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     ],
