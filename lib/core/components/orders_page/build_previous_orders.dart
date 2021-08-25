fde0d1fa253b629b001ba9e8bcead8f21abb6ad5
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-25 18:08:52 +0300

    push

--- lib/core/components/orders_page/build_previous_orders.dart
+++ lib/core/components/orders_page/build_previous_orders.dart
@@ -2,7 +2,25 @@ import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
 BuildPreviousOrders(BuildContext context) {
-  return Center(
-    child: Text("This is previous orders"),
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Card(
+        child: ListTile(
+          title: Text(
+            "Fetucini Alfredo",
+            style: TextStyle(fontSize: 20),
+          ),
+          subtitle: Text(
+            "chicken,parmesan,cream",
+            style: TextStyle(fontSize: 18),
+          ),
+          trailing: Text(
+            "30",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    ],
   );
 }
