b7ee48138c15e77f14f2206f2980d3977a978306
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-26 04:58:45 +0300

    push

--- lib/core/components/orders_page/build_previous_orders.dart
+++ lib/core/components/orders_page/build_previous_orders.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:lego_market_app/core/constant/card/orders_card.dart';
+import 'package:lego_market_app/core/constant/card/previous_orders_card.dart';
 
 // ignore: non_constant_identifier_names
 BuildPreviousOrders(BuildContext context) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: [BuildOrdersCard("Fetucini Alfredo")],
+    children: [BuildPreviousOrdersCard("Fetucini Alfredo")],
   );
 }
