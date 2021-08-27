8fb510758b19e36d1ae40d97b976ceab58de6a8a
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-27 19:17:55 +0300

    push

--- lib/core/constant/card/orders_card.dart
+++ lib/core/constant/card/orders_card.dart
@@ -4,18 +4,18 @@ import 'package:flutter/material.dart';
 BuildOrdersCard(String ordersName) {
   return Card(
     child: ListTile(
-      title: Text(
-        ordersName,
-        style: TextStyle(fontSize: 20),
-      ),
-      subtitle: Text(
-        "continoue",
-        style: TextStyle(fontSize: 18, color: Colors.green.shade700),
-      ),
-      trailing: Text(
-        "30 TL",
-        style: TextStyle(fontSize: 18),
-      ),
-    ),
+        title: Text(
+          ordersName,
+          style: TextStyle(fontSize: 20),
+        ),
+        subtitle: Text(
+          "continues",
+          style: TextStyle(fontSize: 18, color: Colors.green.shade700),
+        ),
+        trailing: Icon(
+          Icons.run_circle_outlined,
+          color: Colors.green,
+          size: 35,
+        )),
   );
 }
