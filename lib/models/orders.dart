5be458803b1144dcac31043ed7dabab205337df7
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-08-29 22:35:03 +0300

    push

--- lib/models/orders.dart
+++ lib/models/orders.dart
@@ -1,3 +1,63 @@
+import 'package:lego_market_app/utils/dbhelper.dart';
+
+class Orders {
+  late int id;
+  late String name;
+  late int price;
+  late String explanation;
+
+  Orders(this.id, this.name, this.explanation, this.price);
+
+  Orders.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    name = map['name'];
+    explanation = map['explanation'];
+    price = map['price'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      DatabaseHelper.columnId: id,
+      DatabaseHelper.columnName: name,
+      DatabaseHelper.columnPrice: price,
+      DatabaseHelper.columnExplanation: explanation,
+    };
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import 'package:lego_market_app/view/orders/orders_screen.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -60,3 +120,5 @@ class Orders {
     this._id = map['id'];
   }
 }
+
+*/
---
+++ lib/utils/dbhelper.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:lego_market_app/models/orders.dart';
+
+class DatabaseHelper {
+  // ignore: unused_field
+  static final _databasename = "ordersdb.db";
+  // ignore: unused_field
+  static final _databaseversion = 1;
+  static final table = 'orders_table';
+  static final columnId = 'id';
+  static final columnName = 'name';
+  static final columnExplanation = 'explanaton';
+  static final columnPrice = 'price';
+
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper order = DatabaseHelper._privateConstructor();
+  // ignore: unused_field
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    String path = join(dbPath, _databasename);
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE $table (
+      $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+      $columnName TEXT NOT NULL,
+      $columnExplanation TEXT NOT NULL,
+      $columnPrice INTEGER NOT NULL, 
+       
+    )
+   ''');
+  }
+
+  Future<int> insert(Orders orders) async {
+    Database db = await order.database;
+    return await db.insert(table, {
+      'name': orders.name,
+      'explanation': orders.explanation,
+      'price': orders.price,
+    });
+  }
+
+  Future<int> update(Orders orders) async {
+    Database db = await order.database;
+    int id = orders.toMap()['id'];
+    return await db
+        .update(table, orders.toMap(), where: '$columnId = ?', whereArgs: [id],);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await order.database;
+    return await db.delete(table, where: '$columnId = ?', whereArgs: [id],);
+  }
+  /*Future<List<Map<String, dynamic>>> queryAllRows() async{
+    Database db = await
+  }*/
+}
+/*
+class OrdersDatabase {
+  static final OrdersDatabase orders = OrdersDatabase._init();
+  // ignore: unused_field
+  static Database? _database;
+  OrdersDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB("orders.db");
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  FutureOr<void> _createDB(Database db, int version) {}
+  Future close() async {
+    final db = await orders.database;
+    db.close();
+  }
+}
+*/
