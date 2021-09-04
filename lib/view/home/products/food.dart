231d291ca109eb4614318e148626af4fe93eb306
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-04 21:41:37 +0300

    push

--- lib/view/home/products/food.dart
+++ lib/view/home/products/food.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lego_market_app/core/components/payment_page/build_payment.dart';
+import 'package:lego_market_app/core/components/home_page/product_list.dart';
 import 'package:lego_market_app/core/constant/scaffold/products_scaffold.dart';
 import 'package:lego_market_app/models/foods.dart';
 import 'package:lego_market_app/utils/dbhelper.dart';
@@ -11,16 +11,16 @@ class FoodList extends StatefulWidget {
 
 class _FoodListState extends State<FoodList> {
   DatabaseHelper databaseHelper;
-  List<Foods> allFoodList;
+  List<Products> allFoodList;
   int id = 1;
   void initState() {
     super.initState();
     // ignore: deprecated_member_use
-    allFoodList = List<Foods>();
+    allFoodList = List<Products>();
     databaseHelper = DatabaseHelper();
     databaseHelper.foodsFetch("Foods").then((allFoodMapList) {
       for (Map readMap in allFoodMapList) {
-        allFoodList.add(Foods.fromMap(readMap));
+        allFoodList.add(Products.fromMap(readMap));
       }
       setState(() {});
     });
@@ -40,50 +40,11 @@ class _FoodListState extends State<FoodList> {
   List<Column> foodItemCreat() {
     return allFoodList
         .map(
-          (foods) => Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BuildPayment(
-                        foods.price,
-                        foods.name,
-                        foods.explanation,
-                      ),
-                    ),
-                  );
-                },
-                child: Column(
-                  children: [
-                    Card(
-                      child: ListTile(
-                        title: Text(
-                          foods.name,
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        subtitle: Text(
-                          foods.explanation,
-                          style: TextStyle(
-                            fontSize: 18,
-                          ),
-                        ),
-                        trailing: Text(
-                          foods.price.toString() + " TL",
-                          style: TextStyle(
-                            fontSize: 19,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+          (foods) => BuildProductList(
+            context,
+            foods.name,
+            foods.explanation,
+            foods.price,
           ),
         )
         .toList();
