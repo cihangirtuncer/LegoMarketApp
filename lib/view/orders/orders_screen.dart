631acfa446996a287e0ffcc0941190aaca2d2cac
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-10 15:43:16 +0300

    push

--- lib/view/orders/orders_screen.dart
+++ lib/view/orders/orders_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lego_market_app/core/components/orders_page/build_orders_screen.dart';
+
+import '../../core/components/scaffold/products_scaffold.dart';
+import '../../models/products.dart';
+import '../../utils/dbhelper.dart';
+import '../home/home_page/product_list.dart';
 
 class OrdersScreen extends StatefulWidget {
   @override
@@ -7,8 +11,42 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  DatabaseHelper databaseHelper;
+  List<Products> allFoodList;
+  int id = 1;
+  void initState() {
+    super.initState();
+    // ignore: deprecated_member_use
+    allFoodList = List<Products>();
+    databaseHelper = DatabaseHelper();
+    databaseHelper.productTable("Orders").then((allFoodMapList) {
+      for (Map readMap in allFoodMapList) {
+        allFoodList.add(Products.fromMap(readMap));
+      }
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BuildOrdersScreen();
+    return BuildProductsScaffold(
+      "ORDERS",
+      ListView(
+        children: foodItemCreat(),
+      ),
+    );
+  }
+
+  List<Column> foodItemCreat() {
+    return allFoodList
+        .map(
+          (product) => BuildProductList(
+            context,
+            product.name,
+            product.explanation,
+            product.price,
+          ),
+        )
+        .toList();
   }
 }
