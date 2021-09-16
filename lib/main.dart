907704bf0ad28664b0471b2b4a514d9e65eca1ed
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-17 00:55:29 +0300

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:lego_market_app/features/model/products.dart';
 
 import 'core/components/app_bar/bottom_navigation_bar.dart';
 
 void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  // Products.firestore = Firestore.instance;
+  //Constants.firestore = Firestore.instance;
   runApp(MyApp());
 }
 
