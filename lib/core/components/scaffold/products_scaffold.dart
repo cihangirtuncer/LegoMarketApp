d6bdac11b4cfbdc9a707754eda2af550330fe5cd
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-22 18:38:43 +0300

    push

--- lib/core/components/scaffold/products_scaffold.dart
+++ lib/core/components/scaffold/products_scaffold.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 // ignore: non_constant_identifier_names
 BuildProductsScaffold(String productName, ListView listView) {
   return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text(
-          productName,
-        ),
+    appBar: AppBar(
+      backgroundColor: Colors.blue.shade900,
+      title: Text(
+        productName,
       ),
-      body: listView);
+    ),
+    body: listView,
+  );
 }
