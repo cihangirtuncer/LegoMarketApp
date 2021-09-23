f15204ed16f74200621966b3ceeb3a9cda2b05da
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-23 19:35:02 +0300

    push

--- lib/core/widget/categories/home_Screen_categories.dart
+++ lib/core/widget/categories/home_Screen_categories.dart
@@ -11,7 +11,7 @@ BuildShoppingContainer(
     onTap: () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => destination),
+        MaterialPageRoute(builder: (context) => destination,),
       );
     },
     child: Container(
