dddaa5800b1eda1399e19b593e6fdc117eb1d71f
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-25 04:23:54 +0300

    push

--- lib/core/components/app_bar/home_app_bar.dart
+++ lib/core/components/app_bar/home_app_bar.dart
@@ -4,7 +4,7 @@ import '../../widget/appbar_title/appbar_title.dart';
 // ignore: non_constant_identifier_names
 AppBar HomeAppBar(BuildContext context) {
   return AppBar(
-    backgroundColor: Colors.blue.shade900,
+    backgroundColor: Colors.indigo.shade900, //Colors.blue.shade900,
     title: BuildHomeAppBarText(),
     actions: [],
   );
