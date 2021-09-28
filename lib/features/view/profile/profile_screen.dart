fdde7abf53c8e3e34d01df983dc956e7314eeb38
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-28 11:32:19 +0300

    push

--- lib/features/view/profile/profile_screen.dart
+++ lib/features/view/profile/profile_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:lego_market_app/core/components/divider/profile_divder.dart';
+import 'package:lego_market_app/core/components/row/profile_row.dart';
 
 import '../../../core/components/app_bar/main_app_bar.dart';
-import 'Profile_page/Profile_screen_build.dart';
+import 'Profile_page/profile_build_data.dart';
 
 class Profile extends StatefulWidget {
+  const Profile({Key key}) : super(key: key);
   @override
   _ProfileState createState() => _ProfileState();
 }
@@ -14,7 +17,81 @@ class _ProfileState extends State<Profile> {
     return Scaffold(
       //backgroundColor: Colors.grey.shade200,
       appBar: BuildAppBar("Profile", false),
-      body: BuildProfile(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          BuildProfileData(
+            Colors.white,
+            80,
+            120,
+            Alignment.center,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    3,
+                    1,
+                    20,
+                    1,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.indigo.shade900,
+                    ),
+                    child: Icon(
+                      Icons.account_circle_rounded,
+                      size: 90,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Text(
+                  "Cihangir Tuncer",
+                  style: TextStyle(
+                    fontSize: 19,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          BuildProfileDivider(1.0),
+          BuildProfileData(
+            Colors.white,
+            80,
+            50,
+            Alignment.centerLeft,
+            BuildProfileRow(
+              "Email: cihangir******@gmail.com",
+              Icons.mail,
+            ),
+          ),
+          BuildProfileDivider(1.0),
+          BuildProfileData(
+            Colors.white,
+            80,
+            50,
+            Alignment.centerLeft,
+            BuildProfileRow(
+              "phone: 0539*******",
+              Icons.phone,
+            ),
+          ),
+          BuildProfileDivider(1.0),
+          BuildProfileData(
+            Colors.white,
+            80,
+            50,
+            Alignment.centerLeft,
+            BuildProfileRow(
+              "address: Lego 1 site / A blok / no: 49",
+              Icons.home,
+            ),
+          ),
+          BuildProfileDivider(1.0),
+        ],
+      ),
     );
   }
 }
