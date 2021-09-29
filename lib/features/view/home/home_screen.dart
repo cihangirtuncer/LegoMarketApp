ed35c9b9bbf79fff9383f8eaa194cd00460b363f
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-09-29 21:13:00 +0300

    push

--- lib/features/view/home/home_screen.dart
+++ lib/features/view/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../../core/components/app_bar/home_app_bar.dart';
+import 'package:lego_market_app/core/components/drawer/drawer.dart';
+import 'package:lego_market_app/core/widget/appbar_title.dart';
 import 'home_page/shopping_options.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,11 +9,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       backgroundColor: Colors.grey.shade300,
-      appBar: HomeAppBar(context),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xF504094E),
+        title: BuildHomeAppBarText(26, 10),
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          onPressed: () => scaffoldKey.currentState!.openDrawer(),
+        ),
+      ),
+      drawer: BuildDrawer(context),
       body: ShoppingOptions(),
     );
   }
