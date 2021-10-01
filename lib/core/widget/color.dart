316228fcfc14952f7848647739ec33bbfa802de3
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-10-01 21:28:05 +0300

    push

--- lib/core/widget/color.dart
+++ lib/core/widget/color.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 // ignore: non_constant_identifier_names
 Color BuildColor() {
-  return Color(0xFFC21903); //0xFFDF001E //0xFFE43307   0xFFCF2204
+  return Color(0xFFDB2108); //0xFFDF001E //0xFFE43307   0xFFCF2204
 }
