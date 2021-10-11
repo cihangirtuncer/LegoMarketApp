2250b31c3af41d775a721c11e796bfcaec7925de
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-10-11 11:59:59 +0300

    push

--- lib/core/widget/appbar_title.dart
+++ lib/core/widget/appbar_title.dart
@@ -7,19 +7,59 @@ Widget BuildHomeAppBarText(double fontsize, double width) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      BuildAppBarText("L", Colors.white, fontsize),
-      BuildAppBarText("E", Colors.white, fontsize),
-      BuildAppBarText("G", Colors.white, fontsize),
-      BuildAppBarText("O", Colors.white, fontsize),
+      BuildAppBarText(
+        "L",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "E",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "G",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "O",
+        Colors.white,
+        fontsize,
+      ),
       SizedBox(
         width: width,
       ),
-      BuildAppBarText("M", Colors.white, fontsize),
-      BuildAppBarText("A", Colors.white, fontsize),
-      BuildAppBarText("R", Colors.white, fontsize),
-      BuildAppBarText("K", Colors.white, fontsize),
-      BuildAppBarText("E", Colors.white, fontsize),
-      BuildAppBarText("T", Colors.white, fontsize),
+      BuildAppBarText(
+        "M",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "A",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "R",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "K",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "E",
+        Colors.white,
+        fontsize,
+      ),
+      BuildAppBarText(
+        "T",
+        Colors.white,
+        fontsize,
+      ),
     ],
   );
 }
