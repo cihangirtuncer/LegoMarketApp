b5169b7ba3c3c24706583e7342ec90930b4b0398
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-10-16 22:13:57 +0300

    push

--- lib/features/view/profile/profile_screen.dart
+++ lib/features/view/profile/profile_screen.dart
@@ -52,147 +52,149 @@ class _ProfileState extends State<Profile> {
             }
             DocumentSnapshot<Object?> data = snapshot.data!;
 
-            return ListView(children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  BuildProfileData(
-                    Colors.transparent,
-                    80,
-                    120,
-                    Alignment.center,
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(
-                            3,
-                            1,
-                            20,
-                            1,
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                10,
+            return ListView(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    BuildProfileData(
+                      Colors.transparent,
+                      80,
+                      120,
+                      Alignment.center,
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                              3,
+                              1,
+                              20,
+                              1,
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  10,
+                                ),
+                                color: Colors.transparent,
                               ),
-                              color: Colors.transparent,
-                            ),
-                            child: Icon(
-                              Icons.account_circle_rounded,
-                              size: 110,
-                              color: Colors.white,
+                              child: Icon(
+                                Icons.account_circle_rounded,
+                                size: 110,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
-                        ),
-                        Text(
-                          data['name surname'],
-                          style: TextStyle(
-                            fontSize: 23,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  BuildProfileData(
-                    Colors.transparent,
-                    80,
-                    50,
-                    Alignment.centerLeft,
-                    buildProfileRow(
-                      data['email'],
-                      Icons.mail,
-                    ),
-                  ),
-                  BuildProfileData(
-                    Colors.transparent,
-                    80,
-                    50,
-                    Alignment.centerLeft,
-                    buildProfileRow(
-                      data['phone'],
-                      Icons.phone,
-                    ),
-                  ),
-                  BuildProfileData(
-                    Colors.transparent,
-                    80,
-                    50,
-                    Alignment.centerLeft,
-                    buildProfileRow(
-                      data['address'],
-                      Icons.home,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: Text(
-                            "Are you sure you want to log out?",
+                          Text(
+                            data['name surname'],
                             style: TextStyle(
+                              fontSize: 23,
+                              color: Colors.white,
                               fontWeight: FontWeight.bold,
-                              fontSize: 17,
                             ),
                           ),
-                          actions: [
-                            TextButton(
-                              onPressed: () async {
-                                await FirebaseAuth.instance.signOut();
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => BottomHomePage(),
-                                  ),
-                                );
-                              },
-                              child: const Text(
-                                'Done',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFFDB2108),
-                                  fontSize: 17,
-                                ),
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.pop(
-                                context,
-                                'NO',
-                              ),
-                              child: const Text(
-                                'Cancel',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF1A9B0E),
-                                  fontSize: 17,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.logout,
-                        color: BuildColor(),
+                        ],
                       ),
-                      title: Text(
-                        "Log out",
-                        style: TextStyle(
-                          fontSize: 20,
+                    ),
+                    BuildProfileData(
+                      Colors.transparent,
+                      80,
+                      50,
+                      Alignment.centerLeft,
+                      buildProfileRow(
+                        data['email'],
+                        Icons.mail,
+                      ),
+                    ),
+                    BuildProfileData(
+                      Colors.transparent,
+                      80,
+                      50,
+                      Alignment.centerLeft,
+                      buildProfileRow(
+                        data['phone'],
+                        Icons.phone,
+                      ),
+                    ),
+                    BuildProfileData(
+                      Colors.transparent,
+                      80,
+                      50,
+                      Alignment.centerLeft,
+                      buildProfileRow(
+                        data['address'],
+                        Icons.home,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () async {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: Text(
+                              "Are you sure you want to log out?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17,
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () async {
+                                  await FirebaseAuth.instance.signOut();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => BottomHomePage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text(
+                                  'Done',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFFDB2108),
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(
+                                  context,
+                                  'NO',
+                                ),
+                                child: const Text(
+                                  'Cancel',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF1A9B0E),
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.logout,
                           color: BuildColor(),
                         ),
+                        title: Text(
+                          "Log out",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: BuildColor(),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ]);
+                  ],
+                ),
+              ],
+            );
           },
         ),
       ),
