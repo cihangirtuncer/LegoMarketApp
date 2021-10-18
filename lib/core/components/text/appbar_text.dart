2c75eda575e04eb42cd181502f56ca610f60d99e
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-10-18 23:52:02 +0300

    push

--- lib/core/components/text/appbar_text.dart
+++ lib/core/components/text/appbar_text.dart
@@ -3,12 +3,14 @@ import 'package:google_fonts/google_fonts.dart';
 
 // ignore: non_constant_identifier_names
 BuildAppBarText(String letter, Color colors, double fontsize) {
-  return Text(letter,
-      style: GoogleFonts.rajdhani(
-        textStyle: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: colors,
-          fontSize: fontsize,
-        ),
-      ));
+  return Text(
+    letter,
+    style: GoogleFonts.rajdhani(
+      textStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: colors,
+        fontSize: fontsize,
+      ),
+    ),
+  );
 }
