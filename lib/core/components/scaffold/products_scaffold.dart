10068adbf2615e5326cbb02286220c1fbde70767
cihangirtuncer <cihangirtuncer184@gmail.com>
2021-10-18 23:50:28 +0300

    push

--- lib/core/components/scaffold/products_scaffold.dart
+++ lib/core/components/scaffold/products_scaffold.dart
@@ -5,11 +5,10 @@ import '../../widget/main_appBar.dart';
 // ignore: non_constant_identifier_names
 BuildProductsScaffold(String productName, ListView listView) {
   return Scaffold(
-    appBar: MainAppBar(
-        Text(
-          productName,
-        ),
-        true),
-    body: BuildGradientContainer(listView),
-  );
+      appBar: MainAppBar(
+          Text(
+            productName,
+          ),
+          true),
+      body: BuildGradientContainer(listView));
 }
