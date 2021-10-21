0df1a31ab6bcb578656b1cd0a83aa7d49df7bf3b
Cihangir Tuncer <85954684+cihangirtuncer@users.noreply.github.com>
2021-10-21 03:38:48 +0300

    push

--- lib/core/widget/appbar_title.dart
+++ lib/core/widget/appbar_title.dart
@@ -4,34 +4,9 @@ import '../components/text/appbar_text.dart';
 
 // ignore: non_constant_identifier_names
 Widget BuildHomeAppBarText(double fontsize, double width) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      BuildAppBarText(
-        "LEGO MA",
-        Colors.white,
-        fontsize,
-      ),
-      BuildAppBarText(
-        "R",
-        Colors.white,
-        fontsize,
-      ),
-      BuildAppBarText(
-        "K",
-        Colors.white,
-        fontsize,
-      ),
-      BuildAppBarText(
-        "E",
-        Colors.white,
-        fontsize,
-      ),
-      BuildAppBarText(
-        "T",
-        Colors.white,
-        fontsize,
-      ),
-    ],
+  return BuildAppBarText(
+    "LEGO MARKET",
+    Colors.white,
+    fontsize,
   );
 }
