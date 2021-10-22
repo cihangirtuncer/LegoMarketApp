34d78cb16de225cbbdd18fde9b92c311a7c849b8
Cihangir Tuncer <85954684+cihangirtuncer@users.noreply.github.com>
2021-10-22 19:18:54 +0300

    push

--- lib/core/widget/appbar_title.dart
+++ lib/core/widget/appbar_title.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import '../components/text/appbar_text.dart';
 
 // ignore: non_constant_identifier_names
-Widget BuildHomeAppBarText(double fontsize, double width) {
+Widget BuildHomeAppBarText(
+  double fontsize,
+  double width,
+) {
   return BuildAppBarText(
     "LEGO MARKET",
     Colors.white,
