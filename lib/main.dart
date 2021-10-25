81d28bb9fcc5bf21a3e4c8e7f0da5aca80fbb077
Cihangir Tuncer <85954684+cihangirtuncer@users.noreply.github.com>
2021-10-25 12:08:09 +0300

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'core/components/app_bar/bottom_navigation_bar.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
