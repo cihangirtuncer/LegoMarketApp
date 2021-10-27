cbc1f2b1bfd6cf60b1813d302b76a399104e345e
Cihangir Tuncer <85954684+cihangirtuncer@users.noreply.github.com>
2021-10-28 00:00:01 +0300

    push

--- lib/core/components/text/appbar_text.dart
+++ lib/core/components/text/appbar_text.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// ignore: non_constant_identifier_names
-BuildAppBarText(String letter, Color colors, double fontsize) {
+buildAppBarText(String letter, Color colors, double fontsize) {
   return Text(
     letter,
     style: GoogleFonts.rajdhani(
