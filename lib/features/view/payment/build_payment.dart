95855b2e96995fbe4795743b4dd88a5d7287570c
Cihangir Tuncer <85954684+cihangirtuncer@users.noreply.github.com>
2021-11-03 19:49:46 +0300

    push

--- lib/features/view/payment/build_payment.dart
+++ lib/features/view/payment/build_payment.dart
@@ -223,3 +223,54 @@ buildPayment(
     ),
   );
 }
+
+/*
+class ButtonPayment extends StatefulWidget {
+  @override
+  _ButtonPaymentState createState() => _ButtonPaymentState();
+}
+
+int volume = 1;
+
+class _ButtonPaymentState extends State<ButtonPayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(
+            Icons.remove_outlined,
+          ),
+          onPressed: () {
+            if (volume > 1) {
+              setState(
+                () {
+                  volume -= 1;
+                },
+              );
+            }
+          },
+        ),
+        Text(
+          "total: $volume",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        IconButton(
+          icon: const Icon(
+            Icons.add_outlined,
+          ),
+          onPressed: () {
+            setState(() {
+              volume += 1;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+*/
