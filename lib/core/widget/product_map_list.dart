698b7d0376c71c2a0d16c79774805de5b0fbf768
Cihangir Tuncer <cihangiraa@cntbilisim.com.tr>
2021-11-11 02:04:24 +0300

    push

--- lib/core/widget/product_map_list.dart
+++ lib/core/widget/product_map_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lego_market_app/product/model/products.dart';
-import 'package:lego_market_app/product/product_list.dart';
+import 'package:lego_market_app/product/product_view_model.dart';
 
 List<Column> foodItemCreat(BuildContext context, List<Products> allFoodList) {
   return allFoodList
